4aa14a3b1a6cd373e62815657c76cacc6d022324
Sonmt <maithanhson@vnpt.vn>
2024-07-19 13:25:30 +0700

    init

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    // Xử lý sự kiện click cho hình ảnh Kangaroo
+    $('#kangaroo').click(function () {
+        var $this = $(this);
+        var startPosition = $this.position().left;
+        var containerWidth = $('.container').width();
+        var imageWidth = $this.width();
+
+        if (startPosition < containerWidth - imageWidth) {
+            $this.animate({ left: containerWidth - imageWidth }, 5000); // Thay đổi thời gian di chuyển nếu cần
+        } else {
+            $this.animate({ left: 0 }, 5000); // Thay đổi thời gian di chuyển nếu cần
+        }
+    });
+
+    // Xử lý sự kiện click cho hình ảnh Swallow
+    $('#swallow').click(function () {
+        var $this = $(this);
+        var startPosition = $this.position().left;
+        var containerWidth = $('.container').width();
+        var imageWidth = $this.width();
+
+        if (startPosition < containerWidth - imageWidth) {
+            $this.animate({ left: containerWidth - imageWidth }, 2380); // Thay đổi thời gian di chuyển nếu cần
+        } else {
+            $this.animate({ left: 0 }, 2380); // Thay đổi thời gian di chuyển nếu cần
+        }
+    });
+
+    // Xử lý sự kiện click cho nút Restart
+    $('#restart').click(function () {
+        $('#kangaroo, #swallow').stop().animate({ left: 0 }, 1000); // Thay đổi thời gian di chuyển nếu cần
+    });
+
+    // Xử lý sự kiện click cho nút Run để di chuyển cả hai hình ảnh
+    $('#run').click(function () {
+        var containerWidth = $('.container').width();
+        var kangarooWidth = $('#kangaroo').width();
+        var swallowWidth = $('#swallow').width();
+        
+        $('#kangaroo').stop().animate({ left: containerWidth - kangarooWidth }, 5000);
+        $('#swallow').stop().animate({ left: containerWidth - swallowWidth }, 2380);
+    });
+
+    // Khởi tạo tooltips với Bootstrap
+    $('[data-toggle="tooltip"]').tooltip();
+});
